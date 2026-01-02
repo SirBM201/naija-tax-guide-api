a4e7f2b72b3609a625aed95f4c689a844ce1c7c1
SirBM201 <bms.concept@hotmail.com>
2026-01-03 01:24:00 +0300

    Create registry.py

---
+++ flows/registry.py
@@ -0,0 +1,14 @@
+FLOW_REGISTRY = {
+    "hustler_onboarding": {
+        "start_step": "ASK_BUSINESS_TYPE",
+        "expiry_hours": 24
+    },
+    "paye": {
+        "start_step": "ASK_MONTHLY_SALARY",
+        "expiry_hours": 12
+    },
+    "vat": {
+        "start_step": "ASK_TURNOVER",
+        "expiry_hours": 12
+    }
+}
