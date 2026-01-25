83cb928b68d95b19ba8c54e9a213f00811f4ad29
SirBM201 <bms.concept@hotmail.com>
2026-01-25 08:52:28 +0300

    Create pricing.py

---
+++ app/core/pricing.py
@@ -0,0 +1,8 @@
+# app/core/pricing.py
+
+# Amounts in Kobo (₦1 = 100 Kobo)
+PLAN_PRICES_KOBO = {
+    "monthly": 3300 * 100,     # ₦3,300
+    "quarterly": 9000 * 100,   # ₦9,000
+    "yearly": 33000 * 100,     # ₦33,000
+}
